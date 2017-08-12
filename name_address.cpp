86a01a08be256ef2c4403ce2be35ecd72440eecb
3stone3 <lei.nie@nokia.com>
2017-08-12 16:11:29 +0800

    Create name_address.cpp
    simple function to print name and address

---
+++ name_address.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+   using namespace std;
+   count << "Your name: ";
+   string name;
+   cin >> name;
+   count << "Your address: ";
+   string addr;
+   cin >> add;
+   count << "Your name and address are: " << name << " and " << add << endl;
+}
